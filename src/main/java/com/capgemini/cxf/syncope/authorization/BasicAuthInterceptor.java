ab036d102ae663acb1126df7cfecd578ae2ed575
Jean-Baptiste Onofré <jbonofre@apache.org>
2014-07-08 10:08:43 +0200

    Return 401 when policy not set to "force" the client to display basic auth popup

--- src/main/java/com/capgemini/cxf/syncope/authorization/BasicAuthInterceptor.java
+++ src/main/java/com/capgemini/cxf/syncope/authorization/BasicAuthInterceptor.java
@@ -1,12 +1,16 @@
 package com.capgemini.cxf.syncope.authorization;
 
+import org.apache.cxf.binding.Binding;
 import org.apache.cxf.configuration.security.AuthorizationPolicy;
 import org.apache.cxf.helpers.DOMUtils;
 import org.apache.cxf.interceptor.Fault;
+import org.apache.cxf.message.Exchange;
 import org.apache.cxf.message.Message;
 import org.apache.cxf.phase.AbstractPhaseInterceptor;
 import org.apache.cxf.phase.Phase;
 import org.apache.cxf.security.SecurityContext;
+import org.apache.cxf.transport.Conduit;
+import org.apache.cxf.ws.addressing.EndpointReferenceType;
 import org.apache.ws.security.WSConstants;
 import org.apache.ws.security.WSUsernameTokenPrincipal;
 import org.apache.ws.security.handler.RequestData;
@@ -17,7 +21,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
 
+import javax.xml.ws.Endpoint;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.security.Principal;
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * This interceptor just get a base authorization, and create a UsernameToken delegated to the Syncope interceptor
@@ -39,13 +49,8 @@ public class BasicAuthInterceptor extends AbstractPhaseInterceptor<Message> {
     public void handleMessage(Message message) throws Fault {
         AuthorizationPolicy policy = message.get(AuthorizationPolicy.class);
         if (policy == null || policy.getUserName() == null || policy.getPassword() == null) {
-            String name = null;
-            if (policy != null) {
-                name = policy.getUserName();
-            }
-            String error = "No user credentials are available";
-            LOGGER.warn("{} for name {}", error, name);
-            throw new SecurityException(error);
+            sendErrorMessage(message, HttpURLConnection.HTTP_UNAUTHORIZED);
+            return;
         }
 
         try {
@@ -70,6 +75,50 @@ public class BasicAuthInterceptor extends AbstractPhaseInterceptor<Message> {
         } catch (Exception ex) {
             throw new Fault(ex);
         }
+    }
+
+    private void sendErrorMessage(Message message, int responseCode) {
+        Message outMessage = getOutMessage(message);
+        outMessage.put(Message.RESPONSE_CODE, responseCode);
+        // set the response headers
+        Map responseHeaders = (Map) message.get(Message.PROTOCOL_HEADERS);
+        if (responseHeaders != null) {
+            responseHeaders.put("WWW-Authenticate", Arrays.asList(new String[]{"Basic realm=realm"}));
+            responseHeaders.put("Content-Length", Arrays.asList(new String[]{ "0" }));
+        }
+        message.getInterceptorChain().abort();
+        try {
+            getConduit(message).prepare(outMessage);
+            close(outMessage);
+        } catch (IOException e) {
+            LOGGER.warn(e.getMessage(), e);
+        }
+    }
+
+    private Message getOutMessage(Message inMessage) {
+        Exchange exchange = inMessage.getExchange();
+        Message outMessage = exchange.getOutMessage();
+        if (outMessage == null) {
+            Endpoint endpoint = exchange.get(Endpoint.class);
+            outMessage = ((Binding) endpoint.getBinding()).createMessage();
+            exchange.setOutMessage(outMessage);
+        }
+        outMessage.putAll(inMessage);
+        return outMessage;
+    }
+
+    private Conduit getConduit(Message inMessage) throws IOException {
+        Exchange exchange = inMessage.getExchange();
+        EndpointReferenceType target = exchange.get(EndpointReferenceType.class);
+        Conduit conduit = exchange.getDestination().getBackChannel(inMessage, null, target);
+        exchange.setConduit(conduit);
+        return conduit;
+    }
+
+    private void close(Message outMessage) throws IOException {
+        OutputStream os = outMessage.getContent(OutputStream.class);
+        os.flush();
+        os.close();
     }
 
     protected UsernameToken convertPolicyToToken(AuthorizationPolicy policy)
